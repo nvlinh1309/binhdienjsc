dbda46743b56cca1305a437769dc6c238945ac75
Linh Nguyen <nvlinh1309@gmail.com>
2023-05-31 11:51:37 +0700

    -save

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,3 +1,7 @@
-<p style = 'text-align:center; bottom:0'>
-    Bản quyền thuộc Google, 2023
-</p>
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023 <a href="#">Binh Dien JSC</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0.0
+    </div>
+  </footer>
--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -5,28 +5,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ $title ?? 'Binh Dien JSC'}}</title>
 
-  <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="/template/admin/plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="/template/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" href="/template/admin/dist/css/adminlte.min.css">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="hold-transition sidebar-mini">
+  <div class="wrapper">
     <x-layouts.header/>
-    <div class="container-fluid pb-3">
-        <div class="d-grid gap-3" style="grid-template-columns: 0fr 2fr;">
-        <x-layouts.nav-left/>
-        
-        <div class="bg-light border rounded-3">
-         {{ $slot }}
+    <x-layouts.sidebar/>
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          {{ $slot }}
         </div>
-        </div>
+      </div>
     </div>
     <x-layouts.footer/>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  </div>
+
+  <script src="/template/admin/plugins/jquery/jquery.min.js"></script>
+  <script src="/template/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/template/admin/dist/js/adminlte.js"></script>
+  <script src="/template/admin/plugins/chart.js/Chart.min.js"></script>
+  <script src="/template/admin/dist/js/demo.js"></script>
+  <script src="/template/admin/dist/js/pages/dashboard3.js"></script>
+  <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+    
 </body>
 </html>
