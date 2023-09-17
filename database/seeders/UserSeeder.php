39deb850bdc5d70017c19867626476bc01c4185d
Linh Nguyen <nvlinh1309@gmail.com>
2023-09-17 23:00:30 +0700

    save

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 
 class UserSeeder extends Seeder
@@ -15,20 +16,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('admins')->insert([
-            [
-                'name' => 'Admin',
-                'email' => 'admin@gmail.com',
-                'password' => bcrypt('admin123'),
-            ]
-        ]);
+        // DB::table('admins')->insert([
+        //     [
+        //         'name' => 'Admin',
+        //         'email' => 'admin@gmail.com',
+        //         'password' => bcrypt('admin123'),
+        //     ]
+        // ]);
 
-        DB::table('users')->insert([
-            [
+        User::create([
                 'name' => 'Quản trị hệ thống',
                 'email' => 'binhdienjscsys@gmail.com',
-                'password' => bcrypt('admin123'),
-            ]
-        ]);
+                'password' => bcrypt('admin123')
+        ])->assignRole('admin');
     }
 }
